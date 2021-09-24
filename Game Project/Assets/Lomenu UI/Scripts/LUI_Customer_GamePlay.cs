776ffceab0818d503af6850f8707612a9cd9062e
89snnfk561 <kenny89002006@gmail.com>
2021-09-24 14:07:32 +0800

    upload Assets/Loumenu UI Folder

---
+++ Game Project/Assets/Lomenu UI/Scripts/LUI_Customer_GamePlay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LUI_Customer_GamePlay : MonoBehaviour
+{
+    
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Game Project/Assets/Lomenu UI/Scripts/LUI_Customer_PlayerCount.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LUI_Customer_PlayerCount : MonoBehaviour
+{
+
+	public Dropdown drop;
+	Resolution[] resolutions;
+
+	void Start()
+	{
+		drop.onValueChanged.AddListener(delegate
+			{
+				Change();
+			}
+		);
+	}
+    private void Change()
+    {
+		Debug.Log(drop.value);
+		GameData.PlayerCount = drop.value + 1;
+    }
+    
+}
